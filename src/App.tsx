d5dcf230cb5823297e56a32b2966beb5cfe9334f
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-05-13 13:34:19 +0200

    ???

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import View from './components/PercentView'
-
-
-export const  App = () => {
-    //const makeColor = (value: number) => `hsl(${(value / 100) * 360}, 100%, 50%)`;
-
+import { useState } from 'react'
+import "./App.css"
+ 
+const App = () => {
+  const [rangeValue, setRangeValue] = useState(100);
+  const initial: number = 50;
+ 
   return (
     <>
-      <View value={100} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
-      <View value={50} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
-      <View value={10} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
+    <div>
+      <input type="range" min={0} max={200} onChange={(e) => {setRangeValue(parseInt(e.target.value))}} defaultValue={initial}/>
+    </div>
+    {console.log(rangeValue)}
+      <View value={rangeValue} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>
     </>
   )
-  
-};
+}
+ 
+export default App
+
+/*
+<ViewBox value={50} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>
+<ViewBox value={25} max={100} makeColor={(p) => `hsl(${p*100},100%,50%)`}/>
+*/
