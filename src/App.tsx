224154b719e60771a9d78d3fffe2f3602e035fed
Nella Pražáková <nella.prazakova.023@pslib.cz>
2025-05-13 13:02:43 +0200

    dalsi barvicky

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import View from './components/PercentView'
 
 
 export const  App = () => {
-    const makeColor = (value: number) => `hsl(${(value / 100) * 360}, 100%, 50%)`;
+    //const makeColor = (value: number) => `hsl(${(value / 100) * 360}, 100%, 50%)`;
 
   return (
     <>
-      <View value={100} max={100} makeColor={(p) => makeColor(p)}/>
-      <View value={50} max={100} makeColor={(p) => makeColor(p)}/>
-      <View value={10} max={100} makeColor={(p) => makeColor(p)} />
+      <View value={100} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
+      <View value={50} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
+      <View value={10} max={100} makeColor={(p) => `hsl(${p * 100}, 100%, 50%)`}/>
     </>
   )
   
